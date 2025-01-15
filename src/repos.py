cb8cf29274fa2a723afac4bea79890d3e8aef7d4
Max <gladkovnissan@gmail.com>
2025-01-15 19:19:33 +0200

    add index.html

--- src/repos.py
+++ src/repos.py
@@ -4,3 +4,7 @@ import os
 
 def get_repos():
     return [repo for repo in os.listdir(".") if os.path.isdir(repo)]
+
+
+def get_last_commit(repo):
+    return datetime.datetime.fromtimestamp(os.path.getmtime(repo))
